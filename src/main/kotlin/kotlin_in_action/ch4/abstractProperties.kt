6a9f987af970d094a2df4b1e72a37d59c81312b8
Iegor Maiorov <maiorov.prog@gmail.com>
2017-07-23 16:46:11 +0300

    chapter4

--- src/main/kotlin/kotlin_in_action/ch4/abstractProperties.kt
+++ src/main/kotlin/kotlin_in_action/ch4/abstractProperties.kt
@@ -6,3 +6,34 @@ interface MyUser {
 
 // interface is allowed to contain abstract properties; lets look at possible implementation
 class PrivateUser(override val nickName: String) : MyUser
+
+class PrivateUser1:MyUser {
+    override val nickName: String = "5"
+}
+
+
+//getter and setter defined for a field
+class PrivateUser2: MyUser {
+    override var nickName: String = "undefined"
+    get() {
+        return field
+    }
+    set(value: String) {
+        field = value
+    }
+}
+
+
+//private setter for a field
+class LengthCounter {
+    var counter: Int = 0
+    private set
+
+    fun addWord(word: String) {
+        counter += word.length
+    }
+}
+
+fun main(args:Array<String>) {
+    val counter = LengthCounter()
+}
